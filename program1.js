b8bb13116aee67927ebea9be3307ecfe76bd7755
mohitlokhande1234 <168068853+mohitlokhande1234@users.noreply.github.com>
2024-04-25 14:09:35 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,6 +1,22 @@
 function longestSubstring(s) {
     // Implementation of longestSubstring function
+    let a_pointer = 0;
+    let b_pointer = 0;
+    let max = 0;
+    const hash_set = new Set();
+    while (b_pointer < s.length) {
+    if (!hash_set.has(s.charAt(b_pointer))) {
+        hash_set.add(s.charAt(b_pointer));
+        b_pointer++;
+        max = Math.max(hash_set.size, max);
+    } else {
+        hash_set.delete(s.charAt(a_pointer));
+        a_pointer++;
+    }
 }
+return max;
+}
+
 
 module.exports = { longestSubstring };
 
